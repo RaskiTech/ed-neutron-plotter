cedcda995270a1e68611fa212804c78068fae442
aarol <aarol@iki.fi>
2025-12-11 00:31:33 +0200

    Add wasm rust module

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,22 @@
-import './style.css'
-import { SearchBox } from './search';
-import { api } from './api';
-import { Galaxy } from './galaxy';
+import "./style.css";
+import { SearchBox } from "./search";
+import { api } from "./api";
+import { Galaxy } from "./galaxy";
+import * as wasm from "../rust-module/pkg";
 
 async function main() {
-
-  const galaxy = new Galaxy()
-  await galaxy.init()
+  const galaxy = new Galaxy();
+  await galaxy.init();
 
   const searchBox = new SearchBox({
     placeholder: "Search stars..",
     onSearch: async (query: string) => {
-      const target = await api.getStarCoords(query)
-      galaxy.setTarget(target)
-    }
-  })
+      const target = await api.getStarCoords(query);
+      galaxy.setTarget(target);
+    },
+  });
 
-  searchBox.mount(document.body)
-
+  searchBox.mount(document.body);
 }
 
-main()
+main();
